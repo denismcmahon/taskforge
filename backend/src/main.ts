48b5389639ea51f52169af52cc622e4eb5923a53
Denis McMahon <denismcmahon@gmail.com>
2025-10-04 16:37:03 +0100

    fix: accept external connections for EC2 instance

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
+    credentials: true,
   });
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
